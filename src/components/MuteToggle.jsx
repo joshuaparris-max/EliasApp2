6c777041cd40cea0403c37a3866d98f41a655333
Josh Parris <joshuaparrisdadlan@gmail.com>
2026-06-02 08:23:55 +1000

    Clean up EliasApp project

--- src/components/MuteToggle.jsx
+++
@@ -1,9 +0,0 @@
-function MuteToggle({ muted, onToggleMute }) {
-  return (
-    <button className="mute-toggle" onClick={onToggleMute} type="button">
-      {muted ? '🔇 Sound Off' : '🔊 Sound On'}
-    </button>
-  );
-}
-
-export default MuteToggle;
